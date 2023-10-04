d2de047b23260bed22c4c608819ccde83510ac51
TherRipper482 <santiagogustavo882@gmail.com>
2023-10-04 09:44:20 -0600

    01 JasvaScript asicrono

---
+++ asicrono.js
@@ -0,0 +1,18 @@
+const teams = ["Chelsie", "Bayern", "Crus Azul"]
+
+console.log('Console 1');
+console.log(teams);
+
+function getTeams(){
+    return teams;
+}
+
+console.log('Console 2');
+console.log(getTeams());
+
+function asyncGetTeams(){
+    setTimeout(() => teams, 2000);
+}
+
+console.log('Console 3');
+console.log(asyncGetTeams());
